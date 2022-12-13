fc6fb0e6e6bc912fa2aec4a03f71d6251851cbd0
baedollee <baesee0401@gmail.com>
2022-12-13 09:32:28 +0900

    12/13

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  type Player = {
+    name: string;
+    age?: number;
+  };
+  const nico: Player = {
+    name: 'nico',
+    age: 23,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>{nico.age}</div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
