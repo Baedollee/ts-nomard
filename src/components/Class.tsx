95d59ec6ef22594ad68d182b78feb812232f5fc4
baedollee <baesee0401@gmail.com>
2022-12-17 10:29:21 +0900

    12/17

--- src/components/Class.tsx
+++ src/components/Class.tsx
@@ -1,16 +1,16 @@
-abstract class User {
-  constructor(
-    private firstName: string,
-    private lastName: string,
-    public nickname: string
-  ) {}
-  getFullNmae() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
+// abstract class User {
+//   constructor(
+//     private firstName: string,
+//     private lastName: string,
+//     public nickname: string
+//   ) {}
+//   getFullNmae() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
 
-class Player extends User {}
+// class Player extends User {}
 
-const bae = new Player('bae', 'kim', 'lee');
+// const bae = new Player('bae', 'kim', 'lee');
 
-bae.getFullNmae();
+// bae.getFullNmae();
