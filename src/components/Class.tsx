8d365a69c3a15531e1d4dd9f1822584dae984e7f
baedollee <baesee0401@gmail.com>
2022-12-17 09:55:22 +0900

    12/17

---
+++ src/components/Class.tsx
@@ -0,0 +1,16 @@
+abstract class User {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+    public nickname: string
+  ) {}
+  getFullNmae() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+class Player extends User {}
+
+const bae = new Player('bae', 'kim', 'lee');
+
+bae.getFullNmae();
--- src/components/Prac.tsx
+++ src/components/Prac.tsx
@@ -20,6 +20,69 @@ const Prac = () => {
 
   const players: [string, number, boolean] = ['nico', 1, true];
 
+  let a: unknown;
+  if (typeof a === 'number') {
+    let b = a + 1;
+  }
+  if (typeof a === 'string') {
+    let b = a.toUpperCase();
+  }
+
+  // void
+  const hello = () => {
+    console.log('x');
+  };
+  // never
+  const hello2 = (name: string | number) => {
+    if (typeof name === 'string') {
+    } else if (typeof name === 'number') {
+    }
+  };
+  type Add = (a: number, b: number) => number;
+
+  const add: Add = (a, b) => {
+    return a + b;
+  };
+
+  // generic
+  // type SuperPrint = {
+  //   (arr: number[]): void;
+  //   (arr: boolean[]): void;
+  //   (arr: string[]): void;
+  //   (arr: (number | boolean)[]): void;
+  // };
+  type SuperPrint = {
+    <T>(arr: T[]): T;
+  };
+  const superPrint: SuperPrint = (arr) => {
+    // arr.forEach((i) => console.log(i));
+    return arr[0];
+  };
+  const c = superPrint([1, 2, 3, 4]);
+  const d = superPrint([true, false, true]);
+  const f = superPrint(['a', 'b', 'c']);
+  const g = superPrint([1, 2, true, false, 'hello']);
+
+  type Player1<T> = {
+    name: string;
+    extraInfo: T;
+  };
+  type BaeExtra = {
+    favFood: string;
+  };
+  type BaePlayer = Player1<BaeExtra>;
+
+  const BaePlayer: BaePlayer = {
+    name: 'bae',
+    extraInfo: {
+      favFood: 'kimch',
+    },
+  };
+  const lynn: Player1<null> = {
+    name: 'lynn',
+    extraInfo: null,
+  };
+
   return (
     <div>
       {nico.age}
